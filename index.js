3410702ce25cd397eff30f2925720454580378d5
ankitarrow <156340025+ankitarrow@users.noreply.github.com>
2024-07-16 16:52:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser')
 const path = require("path");
 const app = express()
 app.use(cors({
-  origin: "https://ankitstore.onrender.com",
+  origin: "https://ankitstore.vercel.app",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   headers: ["Content-Type", "Authorization"]
